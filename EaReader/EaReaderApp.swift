4f32fa50a95d9ad6bbb700601ba9eab85c50b5bc
nekoengineer <negoengineer@icloud.com>
2026-08-02 21:02:30 +0900

    Initial Commit

---
+++ EaReader/EaReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  EaReaderApp.swift
+//  EaReader
+//
+//  Created by Eisuke Nomoto on 2026/08/02.
+//
+
+import SwiftUI
+
+@main
+struct EaReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EaReaderTests/EaReaderTests.swift
@@ -0,0 +1,19 @@
+//
+//  EaReaderTests.swift
+//  EaReaderTests
+//
+//  Created by Eisuke Nomoto on 2026/08/02.
+//
+
+import Testing
+@testable import EaReader
+
+struct EaReaderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
